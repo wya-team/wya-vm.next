210994c78e8e9fb9a3109fdb9d88c37206e1aacf
Mr.Z <zrd0921@qq.com>
2022-07-13 10:05:31 +0800

    fix: *

--- packages/index/index.ts
+++ packages/index/index.ts
@@ -1,9 +1,11 @@
 import { Drag } from '@wya/vm-drag';
+import { Sort } from '@wya/vm-sort';
 import { Widget } from '@wya/vm-widget';
 import { Combo, Preview, PreviewManager } from '@wya/vm-core';
 import { Assist } from '@wya/vm-assist';
 
 export { 
+	Sort,
 	Drag,
 	Assist,
 	Widget,
